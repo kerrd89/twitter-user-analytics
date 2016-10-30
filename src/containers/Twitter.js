6e89e26615b8de319b619fc1d6680dbba695a128
David Kerr <kerrd89@gmail.com>
2016-10-30 15:08:43 -0600

    refactor functions to get Usermentions and hashtags

--- src/containers/Twitter.js
+++ src/containers/Twitter.js
@@ -19,17 +19,76 @@ class List extends Component {
   tweetTemplate(tweet) {
     return (
       <li className="twitter-card" key={tweet.id_str}>
-        <p className="twitter-card-header">
-          {tweet.user.name}<span>@{tweet.user.screen_name}</span>
-        </p>
-        <p className="twitter-card-body">{tweet.text}</p>
-        <p className="twitter-card-footer">{tweet.retweet_count}
-          <span>{tweet.favorited_count}</span>
-          <span>{moment(tweet.created_at).format('LLL')}</span>
-        </p>
+      <p className="twitter-card-header">
+      {tweet.user.name}<span>@{tweet.user.screen_name}</span>
+      </p>
+      <p className="twitter-card-body">{tweet.text}</p>
+      <p className="twitter-card-footer">{tweet.retweet_count}
+      <span>{tweet.favorited_count}</span>
+      <span>{moment(tweet.created_at).format('LLL')}</span>
+      </p>
       </li>
     )
   }
+
+  getUserMentions(tweets) {
+    let userReferences = tweets.map((tweet) => {
+      if(tweet.entities.user_mentions.length !== 0) {
+        let mentions = tweet.entities.user_mentions.map((userMention) => {
+          return userMention.screen_name;
+        });
+        return mentions;
+      }
+    });
+    userReferences = _.countBy(_.compact(_.flatten(userReferences)));
+    let userReferencesFiltered = _.reduce(userReferences, function(result, value, key) {
+      let obj = {};
+      obj.username = key;
+      obj.count = value;
+      if(!result.length) return result.concat(obj);
+      for(let i = 0; i < result.length; i ++) {
+        if(obj.count>result[i].count) {
+          result.splice(i,0,obj);
+          return result;
+        }
+      }
+      return result.concat(obj);
+    }, []);
+    userReferences = _.slice(userReferencesFiltered, 0, 10).map((user)=>{
+      return this.userMentionsTemplate(user);
+    });
+    return userReferences;
+  }
+
+  getHashtags(tweets) {
+    let allHashtags = tweets.map((tweet) => {
+      if(tweet.entities.hashtags.length !== 0) {
+        let tags = tweet.entities.hashtags.map((hashtag) => {
+          return hashtag.text;
+        });
+        return tags;
+      }
+    });
+    allHashtags = _.countBy(_.compact(_.flatten(allHashtags)))
+    let allHashtagsFiltered = _.reduce(allHashtags, function(result, value, key) {
+      let obj = {};
+      obj.hashtag = key;
+      obj.count = value;
+      if(!result.length) return result.concat(obj);
+      for(let i = 0; i < result.length; i ++) {
+        if(obj.count>result[i].count) {
+          result.splice(i,0,obj);
+          return result;
+        }
+      }
+      return result.concat(obj);
+    }, []);
+    allHashtags = _.slice(allHashtagsFiltered, 0, 10).map((hashtag) => {
+      return this.hashtagTemplate(hashtag);
+    });
+    return allHashtags;
+  }
+
 
   render() {
     let tweets;
@@ -42,60 +101,11 @@ class List extends Component {
         return this.tweetTemplate(tweet);
       });
 
-      userMentions = this.props.tweets.map((tweet) => {
-        if(tweet.entities.user_mentions.length !== 0) {
-          let mentions = tweet.entities.user_mentions.map((userMention) => {
-            return userMention.screen_name;
-          });
-          return mentions;
-        }
-      });
-      userMentions = _.countBy(_.compact(_.flatten(userMentions)));
-      let userMentionsFiltered = _.reduce(userMentions, function(result, value, key) {
-        let obj = {};
-        obj.username = key;
-        obj.count = value;
-        if(!result.length) return result.concat(obj);
-        for(let i = 0; i < result.length; i ++) {
-          if(obj.count>result[i].count) {
-            result.splice(i,0,obj);
-            return result;
-          }
-        }
-        return result.concat(obj);
-      }, []);
-      userMentions = _.slice(userMentionsFiltered, 0, 10).map((user)=>{
-        return this.userMentionsTemplate(user);
-      });
+      userMentions = this.getUserMentions(this.props.tweets)
 
+      hashtags = this.getHashtags(this.props.tweets)
 
-      hashtags = this.props.tweets.map((tweet) => {
-        if(tweet.entities.hashtags.length !== 0) {
-          let tags = tweet.entities.hashtags.map((hashtag) => {
-            return hashtag.text;
-          });
-          return tags;
-        }
-      });
-      hashtags = _.countBy(_.compact(_.flatten(hashtags)))
-      let hashtagsFiltered = _.reduce(hashtags, function(result, value, key) {
-        let obj = {};
-        obj.hashtag = key;
-        obj.count = value;
-        if(!result.length) return result.concat(obj);
-        for(let i = 0; i < result.length; i ++) {
-          if(obj.count>result[i].count) {
-            result.splice(i,0,obj);
-            return result;
-          }
-        }
-        return result.concat(obj);
-      }, []);
-      hashtags = _.slice(hashtagsFiltered, 0, 10).map((hashtag) => {
-        return this.hashtagTemplate(hashtag);
-      });
     }
-
 
     return (
       <div>
