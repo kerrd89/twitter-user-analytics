dae2bf7aef79e32798c4995b8b0d9ec81dfb5bf4
David Kerr <kerrd89@gmail.com>
2016-11-01 07:11:32 -0600

    start activity by week

--- src/containers/Twitter.js
+++ src/containers/Twitter.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import moment from 'moment';
-import ChartistGraph from 'react-chartist';
+const LineChart = require('react-chartjs').Line;
 
 class List extends Component {
   constructor(props) {
     super();
     this.state = {
-      simpleLineChartData: {
+      data: {
         labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        series: [[30,30,40,40,50,60,50]]
+        datasets: [{
+          label: "user-activity by day",
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40],
+          spanGaps: false,
+        }
+      ]
       },
-      lineChartOptions: {
-        low: 0,
-        showArea: true
-      }
     };
-  }
-
-  componentWillReceiveProps() {
-    this.getActivityByDate(this.props.tweets);
   }
 
   userMentionsTemplate(userMention) {
@@ -64,11 +77,10 @@ class List extends Component {
     data.push(activityByDate['Friday'])
     data.push(activityByDate['Saturday'])
     data.push(activityByDate['Sunday'])
-    let chartData = this.state.simpleLineChartData
-    chartData.series[0]=data
-    this.state.simpleLineChartData.series[0]=data;
+    let chartData = this.state.data
+    chartData.datasets[0].data = data
     return (
-      <ChartistGraph data={chartData} type='Line'/>
+      <LineChart data={chartData} width="600" height="250"/>
     )
   }
 
